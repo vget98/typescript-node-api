878b5d4e4ae72c1ae4a74dd0cfea176b85b1ffd2
Patrick Truong <pahtruon@gmail.com>
2016-12-07 15:48:05 -0800

    (feat) add end point for finding one hero

--- src/routes/HeroRouter.ts
+++ src/routes/HeroRouter.ts
@@ -16,9 +16,30 @@ export class HeroRouter {
     res.send(Heroes);
   }
 
+  // get one hero by id
+  public getOne(req: Request, res: Response, next: NextFunction) {
+    let query = parseInt(req.params.id);
+    let hero = Heroes.find(hero => hero.id === query);
+    if (hero) {
+      res.status(200)
+        .send({
+          message: 'Success',
+          status: res.status,
+          hero
+        });
+    } else {
+      res.status(404)
+        .send({
+          message: 'No hero found with the given id.',
+          status: res.status
+        });
+    }
+  }
+
   // take each handler and attach to one of the express.router endpoints
   init() {
     this.router.get('/', this.getAll);
+    this.router.get('/:id', this.getOne);
   }
 
 }
